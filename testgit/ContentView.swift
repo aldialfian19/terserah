b5c122f139a67b92244c9ba384aaa456073a8e54
Rinaldi Alfian <rinaldialfian@192.168.1.5>
2022-05-14 21:45:40 +0800

    first commit

---
+++ testgit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testgit
+//
+//  Created by Rinaldi Alfian on 14/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testgit/testgitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testgitApp.swift
+//  testgit
+//
+//  Created by Rinaldi Alfian on 14/05/22.
+//
+
+import SwiftUI
+
+@main
+struct testgitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
